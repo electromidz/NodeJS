ef96905862fc213924814befe60a70bd4f4db9c1
electromidz <electromidz@gmail.com>
2021-05-16 14:52:12 +0430

    done!

--- test3/test.js
+++ test3/test.js
@@ -3,15 +3,17 @@ const EventEmitter = require('events');
 const fs = require('fs');
 
 const myEmitter = new EventEmitter();
+const data = 'Hello Omid';
+var number = 0;
+
+//############## EMITTERS ################################3
 
 myEmitter.on('less', () => {
-    fs.appendFile('omid.txt', 'Hello content!', function (err){
+    fs.appendFile('omid.txt', data, function (err){
         if(err) throw err;
-        console.log('file created!');
     });
+    console.log('file created!');
 });
-
-const data = 'Hello Omid';
 
 myEmitter.on('more', () => {
     process.stdout.write('Your number in greater than 10....! \n');
@@ -20,22 +22,27 @@ myEmitter.on('more', () => {
 myEmitter.on('unlink', () => {
     fs.unlink('omid.txt', (err) => {
         if(err) throw err;
-        console.log('File correctlt deleted!');
-    })
+    });
+    console.log('File correctlt deleted!');
 });
+
+//#######################################################
+
 process.stdout.write('start \n');
 process.stdout.write('let me one numer please from 1 tp 10 ? \n');
-var number = 0;
+
 process.stdin.on('data' , function(data) {
     number = data;
-    process.exit();
-});
-process.on('exit', () => {
     if(number > 10 ){
         myEmitter.emit('more');
     } else if(number < 0 ){
         myEmitter.emit('less');
-    } else if(number === 5 ){
+    } else if(number == 5 ){
         myEmitter.emit('unlink');
     }
+    process.exit();
+});
+
+process.on('exit', () => {
+    process.stdout.write('Process Done and Exit! \n');
 });
