6132cfac162acf1c3c1d08487933f5617cfb4720
electromidz <electromidz@gmail.com>
2021-05-16 14:36:53 +0430

    work with Emmit and File System

--- test3/test.js
+++ test3/test.js
@@ -1,3 +1,28 @@
+
+const EventEmitter = require('events');
+const fs = require('fs');
+
+const myEmitter = new EventEmitter();
+
+myEmitter.on('less', () => {
+    fs.appendFile('omid.txt', 'Hello content!', function (err){
+        if(err) throw err;
+        console.log('file created!');
+    });
+});
+
+const data = 'Hello Omid';
+
+myEmitter.on('more', () => {
+    process.stdout.write('Your number in greater than 10....! \n');
+});
+
+myEmitter.on('unlink', () => {
+    fs.unlink('omid.txt', (err) => {
+        if(err) throw err;
+        console.log('File correctlt deleted!');
+    })
+});
 process.stdout.write('start \n');
 process.stdout.write('let me one numer please from 1 tp 10 ? \n');
 var number = 0;
@@ -7,7 +32,10 @@ process.stdin.on('data' , function(data) {
 });
 process.on('exit', () => {
     if(number > 10 ){
-        process.stdout.write('Your number in greater than 10.... \n');
+        myEmitter.emit('more');
+    } else if(number < 0 ){
+        myEmitter.emit('less');
+    } else if(number === 5 ){
+        myEmitter.emit('unlink');
     }
-})
-
+});
